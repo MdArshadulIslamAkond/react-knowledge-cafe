bc615b087a792c1617b74f159eb3a01b186feb65
Arshadul-Islam <arshadul36@gmail.com>
2024-05-15 11:39:16 +0600

    First creat my react-knowledge-cafe invironment

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Blogs from './compoinents/blogs/blogs'
+import Header from './compoinents/header/header'
+
+function App() {
+  
+  return (
+    <>
+    <Header />
+    <Blogs />
+   </>
+  )
+}
+
+export default App
